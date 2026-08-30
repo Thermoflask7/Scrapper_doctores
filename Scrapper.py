fff86b487c6fe310fed7f32df78ee91f26f1e508
mateo zaragoza <mateozaragozaburruel@gmail.com>
2026-08-29 19:09:09 -0700

    feat: Added doctor profile link scrapping

--- Scrapper.py
+++ Scrapper.py
@@ -1,1 +1,12 @@
-print("helloworld")
+from urllib.request import urlopen 
+from bs4 import BeautifulSoup 
+import re
+
+perfiles = []
+html = urlopen('https://www.doctoralia.com.mx/buscar?q=&loc=San+Carlos')
+bs = BeautifulSoup(html, 'html.parser')
+#print(bs)
+for link in bs.find('div').select("a.text-body"):
+    if '/perfil/' in link.attrs['href']:
+        perfiles.append(link.attrs['href'])
+print(perfiles)
