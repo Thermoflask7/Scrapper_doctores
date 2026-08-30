c7ea91cff04d990ea13c6dbe09f98b6a082925f4
mateo zaragoza <mateozaragozaburruel@gmail.com>
2026-08-29 22:33:17 -0700

    Feat: added data scrapping for clinics and their data

--- Scrapper_info.py
+++ Scrapper_info.py
@@ -12,7 +12,18 @@ from Scrapper_doctores import profile_scrapper
         },
         "doctor2": {
             "nombre": "nombre 1",
-            "clinica": "clinica nombre"
+            "clinicas": [
+            {
+                "nombre_clinica" : nombre,
+                "telefonos" : [telefono 1, telefono 2]
+                "google maps" : www.maps
+            },
+            {
+                "nombre_clinica" : nombre2,
+                "telefonos" : [telefono 1, telefono 2]
+                "google maps" : www.maps
+            }
+            ]
         }
     }
 """
@@ -23,10 +34,33 @@ def info_scrapper():
     for link in links:
         html = urlopen(link)
         bs = BeautifulSoup(html, 'html.parser')
-        #Nombre
-        nombre = (bs.find(attrs={"data-test-id" : "doctor-header-fullname"}).get_text(strip = True))
 
-        #clinica
+        #Nombre
+        nombre = bs.find(attrs={"data-test-id" : "doctor-header-fullname"}).get_text(strip = True)
+
+        #clinicas
+        #clinicas = [clinica.get_text(strip=True) for clinica in bs.find_all(attrs={"data-test-id": "address-info-name"})]
+        clinicas = bs.find_all(attrs={"data-id" : "doctor-address-item"})
+        clinicas_json = []
+        for clinica in clinicas:
+            nombre_tag = clinica.find(attrs={"data-test-id": "address-info-name"})
+            nombre_clinica = nombre_tag.get_text(strip=True) if nombre_tag else "None"
+
+            direccion_tag = clinica.find(attrs={"data-test-id": "address-info-street"})
+            direccion = direccion_tag.get_text(strip=True) if direccion_tag else "None"
+
+            maps = clinica.find('a', href=re.compile('/maps/'))
+            maps_href = maps.attrs['href'] if maps else "None"
+
+            telefonos = [telefono.get_text(strip=True) for telefono in clinica.find_all('b')]
+            clinicas_json.append({
+                "nombre clinica" : nombre_clinica,
+                "direccion" : direccion, 
+                "mapa" : maps_href, 
+                "telefonos" : telefonos
+                })
+            
+        print(clinicas_json)
 
     return()
 info_scrapper()
